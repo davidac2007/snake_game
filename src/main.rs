e7a1adf033dc387cf1dec8201883533cb9479491
David Avendaño <d.avendano.c@hotmail.com>
2021-04-23 12:17:05 -0500

    Creating the snake

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,21 @@ use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 
+use std::collections::LinkedList;
+use std::iter::FromIterator;
+
+#[derive(Clone, PartialEq)]
+
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
 struct Game {
     gl: GlGraphics,
+    snake: Snake,
 }
 impl Game {
     fn render(&mut self, arg: &RenderArgs) {
@@ -22,7 +35,63 @@ impl Game {
 
         self.gl.draw(arg.viewport(), |_c, gl| {
             graphics::clear(GREEN, gl);
-        })
+        });
+
+        self.snake.render(&mut self.gl, arg);
+    }
+
+    fn update(&mut self) {
+        self.snake.update();
+    }
+
+    fn pressed(&mut self, btn: &Button) {
+        let last_direction = self.snake.dir.clone();
+        self.snake.dir = match btn {
+            &Button::Keyboard(Key::Up) if last_direction != Direction::Down => Direction::Up,
+            &Button::Keyboard(Key::Down) if last_direction != Direction::Up => Direction::Down,
+            &Button::Keyboard(Key::Left) if last_direction != Direction::Right => Direction::Left,
+            &Button::Keyboard(Key::Right) if last_direction != Direction::Left => Direction::Right,
+            _ => last_direction,
+        };
+    }
+}
+
+struct Snake {
+    body: LinkedList<(i32, i32)>,
+    dir: Direction,
+}
+
+impl Snake {
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        use graphics;
+
+        let RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let squares = Vec<graphics::types::Rectangle> = self.body.iter().map(|&(x, y)|{
+          
+            graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64)
+
+        }).collect();
+
+        
+        gl.draw(args.viewport(), |_c, gl| {
+            let transform = _c.transform;
+            squares.into_iter().for_each(|square|  graphics::rectangle(RED, square, transform, gl)
+           );
+        });
+    }
+    fn update(&mut self) {
+        let mut new_head = (*self.body.front().expect("Snake has no body")).clone();
+
+        match self.dir {
+            Direction::Left => new_head.0 -= 1,
+            Direction::Right => new_head.0 += 1,
+            Direction::Up => new_head.1 -= 1,
+            Direction::Down => new_head.1 += 1,
+        }
+
+        self.body.push_front(new_head);
+        self.body.pop_back().unwrap();
     }
 }
 
@@ -37,12 +106,20 @@ fn main() {
 
     let mut game = Game {
         gl: GlGraphics::new(opengl),
+        snake: Snake {
+            body: LinkedList::from_iter((vec![(0,0), (0,1)]).into_iter()),
+            dir: Direction::Right,
+        },
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).ups(8);
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             game.render(&args);
+        }
+
+        if let Some(u) = e.update_args() {
+            game.update();
         }
     }
 }
